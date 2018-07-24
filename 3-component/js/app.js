fac3d3fa12b281ef3d498a0488f561e19e529bdb
JangJur <wkdwndud753@naver.com>
2018-07-24 10:25:12 -0700

    Scafolding-addFormComponent
    1. scafolding
    2. create FormComponent

---
+++ 3-component/js/app.js
@@ -0,0 +1,71 @@
+import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
+import HistoryModel from './models/HistoryModel.js'
+
+import FormComponent from './components/FormComponent.js'
+
+new Vue({
+    el: '#app',
+    data: {
+        query: '',
+        submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
+        keywords: [],
+        history: [],
+        searchResult: [],
+    },
+    components: {
+        'search-form': FormComponent
+    },
+    created() {
+        this.selectedTab = this.tabs[0]
+        this.fetchKeyword()
+        this.fetchHistory()
+    },
+    methods: {
+        onSubmit(e) {
+            this.search()
+        },
+        onKeyup(e) {
+            if(!this.query.length) this.resetForm()
+        },
+        onReset(e) {
+            this.resetForm()
+        },
+        onClickTab(tab) {
+            this.selectedTab = tab
+        },
+        onClickKeyword(keyword) {
+            this.query = keyword
+            this.search()
+        },
+        fetchKeyword() {
+          KeywordModel.list().then(data => {
+              this.keywords = data
+          })  
+        },
+        onClickRemoveHistory(keyword) {
+            HistoryModel.remove(keyword)
+            this.fetchHistory()
+        },
+        fetchHistory() {
+            HistoryModel.list().then(data => {
+                this.history = data
+            })
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+            HistoryModel.add(this.query)
+            this.fetchHistory()
+        },
+        resetForm() {
+            this.query = ''
+            this.submitted = false
+            this.searchResult = []
+        }
+    }
+})
